10d1609a6f654bb3008944b22fdbf75c9dcac235
ajay8106 <52490796+ajay8106@users.noreply.github.com>
2020-05-04 09:33:51 +0530

    Update ann.py

--- ann.py
+++ ann.py
@@ -1,22 +1,8 @@
-# Artificial Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+#  dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
@@ -36,7 +22,7 @@ X = onehotencoder.fit_transform(X)
 X.toarray()
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
+# Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
@@ -44,11 +30,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Part 2 - Now let's make the ANN!
-
-# Importing the Keras libraries and packages
+X_test = sc.transform(X_test 
+                      
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
@@ -66,18 +49,16 @@ classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 're
 # Adding the output layer
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
-# Compiling the ANN
+# Compiling
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-# Fitting the ANN to the Training set
+# Fitting
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
 
-# Part 3 - Making predictions and evaluating the model
-
-# Predicting the Test set results
+# Predicting 
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
 
-# Making the Confusion Matrix
+# Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
